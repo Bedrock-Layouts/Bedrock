4c39a1271c663d09aae044cada5a4946535f3962
Jarvis1010 <travis.mair@gmail.com>
2020-03-20 09:31:16 -0600

    define spacing

--- packages/spacing-constants/src/index.tsx
+++ packages/spacing-constants/src/index.tsx
@@ -1,1 +1,15 @@
-export const spacing1x = '0.5rem';
+const xs = '0.25rem';
+const sm = '0.25rem';
+const med = '0.5rem';
+const lg = '1rem';
+const xl = '2rem';
+const xxl = '4rem';
+
+export const spacing = {
+  xs,
+  sm,
+  med,
+  lg,
+  xl,
+  xxl,
+};
