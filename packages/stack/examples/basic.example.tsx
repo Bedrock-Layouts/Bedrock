3296b44481fb60033b27e3f51bacfac8d94eddcc
Jarvis1010 <travis.mair@gmail.com>
2020-03-20 21:49:21 -0600

    Basic Stack Component Stories

---
+++ packages/stack/examples/basic.example.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Stack from '@bedrock-layout/stack';
+
+let name = 'Basic';
+
+function Example() {
+  return (
+    <Stack>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+        consequuntur corrupti beatae commodi vitae, perspiciatis totam provident
+        architecto doloribus aperiam sapiente, incidunt nihil suscipit
+        voluptatibus tempore est dolor! Iusto, vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+        consequuntur corrupti beatae commodi vitae, perspiciatis totam provident
+        architecto doloribus aperiam sapiente, incidunt nihil suscipit
+        voluptatibus tempore est dolor! Iusto, vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+        consequuntur corrupti beatae commodi vitae, perspiciatis totam provident
+        architecto doloribus aperiam sapiente, incidunt nihil suscipit
+        voluptatibus tempore est dolor! Iusto, vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+        consequuntur corrupti beatae commodi vitae, perspiciatis totam provident
+        architecto doloribus aperiam sapiente, incidunt nihil suscipit
+        voluptatibus tempore est dolor! Iusto, vero.
+      </p>
+    </Stack>
+  );
+}
+
+Example.story = { name };
+export const Comp = Example;
+export default { title: 'Stack' };
--- packages/stack/src/index.tsx
+++ packages/stack/src/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import {
   spacing as spacingMap,
   SpacingTypes,
@@ -8,9 +9,18 @@ export interface StackProps {
   gutter?: SpacingTypes;
 }
 
-export default styled.div<StackProps>`
+const Stack = styled.div<StackProps>`
   display: grid;
   grid-auto-columns: 100%;
   grid-gap: ${({ gutter, theme: { spacing = spacingMap } }) =>
     gutter ? spacing[gutter] : spacing.md};
 `;
+
+if (__DEV__) {
+  Stack.displayName = 'Stack';
+  Stack.propTypes = {
+    gutter: PropTypes.oneOf(['none', 'xs', 'sm', 'ms', 'lg', 'xl', 'xxl']),
+  };
+}
+
+export default Stack;
