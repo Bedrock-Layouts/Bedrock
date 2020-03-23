e42f359ae62ad70a7f58baa773d386658f4f7387
Jarvis1010 <travis.mair@gmail.com>
2020-03-22 22:15:52 -0600

    setup jest

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+  ],
+  env: {
+    test: {
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+        "require-context-hook",
+      ],
+    },
+  },
+};
