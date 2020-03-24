8551e7b3d86dc10f84293cdf0c47b52af74e933a
Jarvis1010 <travis.mair@gmail.com>
2020-03-24 01:01:27 -0600

    improve logo

--- src/gatsby-theme-docz/components/Logo/index.js
+++ src/gatsby-theme-docz/components/Logo/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Brand from './logo.js';
 import styled from 'styled-components';
 
 const Split = styled.div`
@@ -17,6 +16,21 @@ const Title = styled.span`
     white-space: normal;
   }
 `;
+
+const Brand = () => {
+  return (
+    <svg width='60px' height='42px' viewBox='0 0 400 244.224'>
+      <g>
+        <path
+          fill='currentColor'
+          fillRule='evenodd'
+          stroke='none'
+          d='M0 99.01v95.049h236.304v50.165H396.04V3.96H166.337v48.845h14.442l.37-16.831.369-16.832h199.34l.338 106.613.338 106.613-65.025-.342-65.024-.343-.342-68.977-.341-68.977h106.954V77.888H122.772V60.726h-14.521v17.162H46.205V93.729h190.099v85.809H14.501l.34-80.198.341-80.198H107.591l.402 6.271.402 6.27H122.772V3.96H0v95.05'
+        ></path>
+      </g>
+    </svg>
+  );
+};
 
 export const Logo = () => {
   return (
