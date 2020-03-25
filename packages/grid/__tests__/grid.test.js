38a94d15db3008414d7b01342b79d24c33275f53
Jarvis1010 <travis.mair@gmail.com>
2020-03-25 15:03:43 -0600

    initial grid commit

---
+++ packages/grid/__tests__/grid.test.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { spacing } from '@bedrock-layout/spacing-constants';
+import { ThemeProvider } from 'styled-components';
+import Grid from '../src';
+
+const Lorem = () => (
+  <>
+    {Array.from(Array(4).keys()).map(i => (
+      <p key={i}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+        vestibulum tortor, vitae venenatis lectus. Praesent gravida dapibus
+        neque sit amet molestie. Morbi blandit eu dolor a luctus. Vestibulum
+        sollicitudin elit ac nunc scelerisque rhoncus. Nulla felis sapien,
+        condimentum ut imperdiet vel, aliquet id ante. Pellentesque habitant
+        morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        Quisque ultrices, quam nec scelerisque malesuada, lectus elit semper
+        diam, ac placerat purus tortor et enim.
+      </p>
+    ))}
+  </>
+);
+
+describe('Grid', () => {
+  describe('correct usage', () => {
+    test('Grid is not null', () => {
+      expect(Grid).toBeTruthy();
+    });
+
+    it('renders default gutters', () => {
+      const grid = create(
+        <Grid>
+          <Lorem />
+        </Grid>
+      );
+      expect(grid.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders all the gutter options', () => {
+      Object.keys(spacing).forEach(gutter => {
+        const grid = create(
+          <Grid gutter={gutter}>
+            <Lorem />
+          </Grid>
+        );
+        expect(grid.toJSON()).toMatchSnapshot();
+      });
+    });
+
+    it('renders custom minItemWidth', () => {
+      const grid = create(
+        <Grid minItemWidth={320}>
+          <Lorem />
+        </Grid>
+      );
+      expect(grid.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders with theme overrides', () => {
+      const grid = create(
+        <ThemeProvider theme={{ spacing: { md: '200px' } }}>
+          <Grid>
+            <Lorem />
+          </Grid>
+        </ThemeProvider>
+      );
+      expect(grid.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('incorrect usage', () => {
+    let originalError;
+    let spy;
+    beforeEach(() => {
+      originalError = console.error;
+      spy = jest.fn();
+      console.error = spy;
+    });
+    afterEach(() => {
+      console.error = originalError;
+    });
+
+    it('renders default with console error with wrong gutter input', () => {
+      expect(spy.mock.calls.length).toBe(0);
+
+      const errorStack = create(
+        <Grid gutter='incorrect'>
+          <Lorem />
+        </Grid>
+      );
+
+      expect(spy.mock.calls.length).toBe(1);
+      expect(errorStack.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders default with console error with minItemWidth input', () => {
+      expect(spy.mock.calls.length).toBe(0);
+
+      const errorStack = create(
+        <Grid minItemWidth='incorrect'>
+          <Lorem />
+        </Grid>
+      );
+
+      expect(spy.mock.calls.length).toBe(1);
+      expect(errorStack.toJSON()).toMatchSnapshot();
+    });
+  });
+});
--- packages/grid/src/index.tsx
+++ packages/grid/src/index.tsx
@@ -5,46 +5,55 @@ import {
   SpacingTypes,
 } from '@bedrock-layout/spacing-constants';
 
-export interface StackProps {
+export interface GridProps {
   gutter?: SpacingTypes;
+  minItemWidth?: number;
 }
 
-const Stack = styled.div<StackProps>`
+const Grid = styled.div<GridProps>`
   box-sizing: border-box;
+
   --gutter: ${({ gutter, theme: { spacing = {} } }) => {
     const spacingMap = { ...defaultSpacings, ...spacing };
     return gutter && spacingMap[gutter] ? spacingMap[gutter] : spacingMap.md;
   }};
 
+--minItemWidth:${props =>
+  typeof props.minItemWidth === 'number' ? props.minItemWidth : 1}px;
+
   display: grid;
-  grid-auto-columns: 100%;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(var(--minItemWidth), 1fr)
+  );
   grid-gap: var(--gutter);
 
+  @supports (width: min(${props => props.minItemWidth}px, 100%)) {
+    grid-template-columns: repeat(auto-fit, minmax(min(var(--minItemWidth), 100%), 1fr));
+  }
+
   @supports not (grid-gap: var(--gutter)) {
-    display: flex;
+    /* display: flex;
     flex-flow: column;
 
     & > * + * {
-      margin-top: ${({ gutter, theme: { spacing = {} } }) => {
-        const spacingMap = { ...defaultSpacings, ...spacing };
-        return gutter && spacingMap[gutter]
-          ? spacingMap[gutter]
-          : spacingMap.md;
-      }};
+      margin-top: var(--gutter);
     }
-  }
+  } */
 `;
 
-Stack.displayName = 'Stack';
+Grid.displayName = 'Grid';
 
-Stack.propTypes = {
+Grid.propTypes = {
   gutter: PropTypes.oneOf<SpacingTypes>(
     Object.keys(defaultSpacings) as SpacingTypes[]
   ),
+  minItemWidth: PropTypes.number,
 };
 
-Stack.defaultProps = {
+Grid.defaultProps = {
   gutter: 'md',
+  minItemWidth: 1,
 };
 
-export default Stack;
+export default Grid;
