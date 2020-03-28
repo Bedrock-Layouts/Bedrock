ae6d9e26a10ffc7924d56d088e629e3a337d5e09
Jarvis1010 <travis.mair@gmail.com>
2020-03-27 22:46:22 -0600

    add happy test

---
+++ packages/columns/__tests__/columns.test.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { spacing } from '@bedrock-layout/spacing-constants';
+import { ThemeProvider } from 'styled-components';
+import Columns from '../src';
+
+const Lorem = () => (
+  <>
+    {Array.from(Array(4).keys()).map(i => (
+      <p key={i}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+        vestibulum tortor, vitae venenatis lectus. Praesent gravida dapibus
+        neque sit amet molestie. Morbi blandit eu dolor a luctus. Vestibulum
+        sollicitudin elit ac nunc scelerisque rhoncus. Nulla felis sapien,
+        condimentum ut imperdiet vel, aliquet id ante. Pellentesque habitant
+        morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        Quisque ultrices, quam nec scelerisque malesuada, lectus elit semper
+        diam, ac placerat purus tortor et enim.
+      </p>
+    ))}
+  </>
+);
+
+describe('Columns', () => {
+  describe('correct usage', () => {
+    test('Columns is not null', () => {
+      expect(Columns).toBeTruthy();
+    });
+
+    it('renders default gutters', () => {
+      const columns = create(
+        <Columns>
+          <Lorem />
+        </Columns>
+      );
+      expect(columns.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders all the gutter options', () => {
+      Object.keys(spacing).forEach(gutter => {
+        const columns = create(
+          <Columns gutter={gutter}>
+            <Lorem />
+          </Columns>
+        );
+        expect(columns.toJSON()).toMatchSnapshot();
+      });
+    });
+
+    it('renders custom columns', () => {
+      const columns = create(
+        <Columns columns={5}>
+          <Lorem />
+        </Columns>
+      );
+      expect(columns.toJSON()).toMatchSnapshot();
+    });
+
+    // it('renders with theme overrides', () => {
+    //   const Columns = create(
+    //     <ThemeProvider theme={{ spacing: { md: '200px' } }}>
+    //       <Columns>
+    //         <Lorem />
+    //       </Columns>
+    //     </ThemeProvider>
+    //   );
+    //   expect(Columns.toJSON()).toMatchSnapshot();
+    // });
+  });
+
+  describe('incorrect usage', () => {
+    let originalError;
+    let spy;
+    beforeEach(() => {
+      originalError = console.error;
+      spy = jest.fn();
+      console.error = spy;
+    });
+    afterEach(() => {
+      console.error = originalError;
+    });
+
+    it('renders default with console error with wrong gutter input', () => {
+      expect(spy.mock.calls.length).toBe(0);
+
+      const errorStack = create(
+        <Columns gutter='incorrect'>
+          <Lorem />
+        </Columns>
+      );
+
+      expect(spy.mock.calls.length).toBe(1);
+      expect(errorStack.toJSON()).toMatchSnapshot();
+    });
+
+    // it('renders default with console error with minItemWidth input', () => {
+    //   expect(spy.mock.calls.length).toBe(0);
+
+    //   const errorStack = create(
+    //     <Columns minItemWidth='incorrect'>
+    //       <Lorem />
+    //     </Columns>
+    //   );
+
+    //   expect(spy.mock.calls.length).toBe(1);
+    //   expect(errorStack.toJSON()).toMatchSnapshot();
+    // });
+  });
+});
