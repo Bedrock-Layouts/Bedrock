4453c5ece6691ce64958225879d5482450df0a7f
Jarvis1010 <travis.mair@gmail.com>
2020-03-30 00:10:23 -0600

    add tests

---
+++ packages/center/__tests__/center.test.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components';
+import Center from '../src';
+
+const Lorem = () => (
+  <>
+    {Array.from(Array(4).keys()).map(i => (
+      <p key={i}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+        vestibulum tortor, vitae venenatis lectus. Praesent gravida dapibus
+        neque sit amet molestie. Morbi blandit eu dolor a luctus. Vestibulum
+        sollicitudin elit ac nunc scelerisque rhoncus. Nulla felis sapien,
+        condimentum ut imperdiet vel, aliquet id ante. Pellentesque habitant
+        morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        Quisque ultrices, quam nec scelerisque malesuada, lectus elit semper
+        diam, ac placerat purus tortor et enim.
+      </p>
+    ))}
+  </>
+);
+
+describe('Center', () => {
+  describe('correct usage', () => {
+    test('Center is not null', () => {
+      expect(Center).toBeTruthy();
+    });
+
+    it('renders default width', () => {
+      const center = create(
+        <Center>
+          <Lorem />
+        </Center>
+      );
+      expect(center.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders custom width', () => {
+      const center = create(
+        <Center maxWidth={320}>
+          <Lorem />
+        </Center>
+      );
+      expect(center.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders with theme overrides', () => {
+      const center = create(
+        <ThemeProvider theme={{ breakPoints: { medium: '1600px' } }}>
+          <Center>
+            <Lorem />
+          </Center>
+        </ThemeProvider>
+      );
+      expect(center.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('incorrect usage', () => {
+    let originalError;
+    let spy;
+    beforeEach(() => {
+      originalError = console.error;
+      spy = jest.fn();
+      console.error = spy;
+    });
+    afterEach(() => {
+      console.error = originalError;
+    });
+
+    it('renders default with console error with no children', () => {
+      expect(spy.mock.calls.length).toBe(0);
+
+      const errorStack = create(
+        <Center maxWidth='incorrect'>
+          <Lorem />
+        </Center>
+      );
+
+      expect(spy.mock.calls.length).toBe(1);
+      expect(errorStack.toJSON()).toMatchSnapshot();
+    });
+  });
+});
--- packages/center/src/index.tsx
+++ packages/center/src/index.tsx
@@ -1,45 +1,38 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import PropTypes from 'prop-types';
-// import {
-//   breakPoints as defaultBreakpoints,
-//   BreakPoints,
-// } from '@bedrock-layout/spacing-constants';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import {
+  breakPoints as defaultBreakpoints,
+  BreakPoints,
+} from '@bedrock-layout/spacing-constants';
 
-// type MergeBreakpoints = (breakPoints: object) => BreakPoints;
-// const mergeBreakpoints: MergeBreakpoints = (breakPoints = {}) => ({
-//   ...defaultBreakpoints,
-//   ...breakPoints,
-// });
+type MergeBreakpoints = (breakPoints: object) => BreakPoints;
+const mergeBreakpoints: MergeBreakpoints = (breakPoints = {}) => ({
+  ...defaultBreakpoints,
+  ...breakPoints,
+});
 
-// const Outer = styled.div`
-//   padding: 0;
-//   max-width: 100%;
-//   overflow: hidden;
-//   height: 100%;
-// `;
+export interface CenterProps {
+  maxWidth?: number;
+}
 
-// const Inner = styled.div`
-//   margin: 0 auto;
-//   padding: 0 0;
-//   max-width: ${props => mergeBreakpoints(props.theme.breakPoints).xxlarge};
-// `;
+const Center = styled.div<CenterProps>`
+  --maxWidth: ${({ maxWidth, theme: { breakPoints } }) =>
+    typeof maxWidth === 'number'
+      ? `${maxWidth}px`
+      : mergeBreakpoints(breakPoints).medium};
 
-// export interface AppBoundaryProps {
-//   children: React.ReactNode;
-// }
+  box-sizing: content-box;
+  margin-left: auto;
+  margin-right: auto;
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+  max-width: var(--maxWidth);
+`;
 
-// const AppBoundary = ({ children }: AppBoundaryProps) => (
-//   <Outer>
-//     <Inner>{children}</Inner>
-//   </Outer>
-// );
+Center.displayName = 'Center';
 
-// AppBoundary.displayName = 'AppBoundary';
+Center.propTypes = {
+  maxWidth: PropTypes.number,
+};
 
-// AppBoundary.propTypes = {
-//   children: PropTypes.element.isRequired,
-// };
-
-// export default AppBoundary;
-export default () => null;
+export default Center;
