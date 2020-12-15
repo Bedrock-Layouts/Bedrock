98fd8ac0bd55e22880fab8c73f839c49db4cc883
Jarvis1010 <travis.mair@gmail.com>
2020-12-14 21:51:45 -0700

    remove from git

--- packages/reel/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface ReelProps {
-    maxHeight?: number;
-    maxWidth?: number;
-    snapX?: boolean;
-    proximity?: boolean;
-}
-declare const Reel: import("styled-components").StyledComponent<"div", any, ReelProps, never>;
-export default Reel;
