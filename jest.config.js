96c6079d7f656a0f9e5d2522f340ca349c30c7bd
Jarvis1010 <travis.mair@gmail.com>
2021-05-09 00:49:37 -0600

    perf(masonry-grid): use a singleton pattern for resize observer
    using a resize observer as a singleton improves performance
    
    #511

--- jest.config.js
+++ jest.config.js
@@ -59,10 +59,10 @@ module.exports = {
       lines: 73,
     },
     "./packages/masonry-grid/src/index.tsx": {
-      statements: 58,
+      statements: 55,
       branches: 31,
-      functions: 75,
-      lines: 61,
+      functions: 64,
+      lines: 58,
     },
     "./packages/switcher/src/index.tsx": {
       statements: 95,
