d0e53b940e468b4b33671048937b34b6ef0970cb
Jarvis1010 <travis.mair@gmail.com>
2021-08-31 00:17:37 -0600

    docs(columns): update docs to standard

---
+++ packages/columns/examples/argTypes.ts
@@ -0,0 +1,77 @@
+export const columnsArgTypes = {
+  gutter: {
+    description: "Sets space between each element",
+    type: { name: "string", required: true },
+    table: {
+      type: { summary: "string" },
+    },
+    control: "select",
+    options: [
+      "none",
+      "xxs",
+      "xs",
+      "sm",
+      "md",
+      "mdLg",
+      "lg",
+      "lgXl",
+      "xl",
+      "xlXXl",
+      "xxl",
+    ],
+  },
+  columns: {
+    description: "Sets the number of columns",
+    type: { name: "number" },
+    table: {
+      type: { summary: "number" },
+    },
+    control: "number",
+  },
+  dense: {
+    description: "Toggle for is the layout should use a dense layout or not",
+    defaultValue: false,
+    type: { name: "boolean" },
+    table: {
+      type: { summary: "boolean" },
+    },
+    control: "boolean",
+  },
+  switchAt: {
+    description: "Toggle for is the layout should use a dense layout or not",
+    type: { name: "string" },
+    table: {
+      type: { summary: "css length as string or number" },
+    },
+    control: "text",
+  },
+};
+
+export const columnArgTypes = {
+  span: {
+    description: "Sets the number of columns the element will span",
+    type: { name: "number" },
+    table: {
+      type: { summary: "number" },
+    },
+    control: "number",
+  },
+  offsetStart: {
+    description:
+      "Sets the number of columns the element will offset before the element",
+    type: { name: "number" },
+    table: {
+      type: { summary: "number" },
+    },
+    control: "number",
+  },
+  offsetEnd: {
+    description:
+      "Sets the number of columns the element will offset after the element",
+    type: { name: "number" },
+    table: {
+      type: { summary: "number" },
+    },
+    control: "number",
+  },
+};
