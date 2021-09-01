afe12433fd5b5da8bd8fa5dd32c3119a35615a60
Jarvis1010 <travis.mair@gmail.com>
2021-09-01 16:11:40 -0600

    docs(landing page): add the beginning of new website flow

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -7,8 +7,9 @@ addons.setConfig({
   theme: create({
     base: "light",
     brandTitle: "Bedrock Layout Primitives",
-    brandUrl: "https://bedrock-layout.dev",
+    brandUrl: "/",
     brandImage:
       "https://user-images.githubusercontent.com/5460770/77477816-8df68000-6de2-11ea-83be-9f12c8de7f0d.png",
+    textColor: "#161e2e",
   }),
 });
