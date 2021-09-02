6ae654f22d1e4ba2175c1d261d144422c7c29639
Jarvis1010 <travis.mair@gmail.com>
2021-09-02 02:03:38 -0600

    docs(cover): update docs for cover component

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -11,5 +11,6 @@ addons.setConfig({
     brandImage:
       "https://user-images.githubusercontent.com/5460770/77477816-8df68000-6de2-11ea-83be-9f12c8de7f0d.png",
     textColor: "#161e2e",
+    fontBase: '"Roboto", sans-serif',
   }),
 });
---
+++ packages/cover/examples/argTypes.ts
@@ -0,0 +1,49 @@
+export const argTypes = {
+  gutter: {
+    description: "Sets space between each element",
+    type: { name: "string", required: true },
+    table: {
+      type: { summary: "string" },
+    },
+    control: "select",
+    options: [
+      "none",
+      "xxs",
+      "xs",
+      "sm",
+      "md",
+      "mdLg",
+      "lg",
+      "lgXl",
+      "xl",
+      "xlXXl",
+      "xxl",
+    ],
+  },
+  minHeight: {
+    description: "Sets the minimum block size of the component",
+    type: { name: "string" },
+    defaultValue: "100vh",
+    table: {
+      type: { summary: "css length-percentage" },
+      defaultValue: { summary: "100vh" },
+    },
+    control: "text",
+  },
+  top: {
+    description: "renders an React Node before the children",
+    type: { name: "React Node" },
+    defaultValue: "100vh",
+    table: {
+      type: { summary: "React Node" },
+    },
+  },
+  bottom: {
+    description: "renders an React Node after the children",
+    type: { name: "React Node" },
+    defaultValue: "100vh",
+    table: {
+      type: { summary: "React Node" },
+    },
+  },
+};
