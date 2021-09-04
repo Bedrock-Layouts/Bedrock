4021990efc542a6622d6fa987a4085d4cfa5b8cb
Jarvis1010 <travis.mair@gmail.com>
2021-09-03 19:10:45 -0600

    build(package.json): fix ts problem

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
-const { TsConfigPathsPlugin } = require("awesome-typescript-loader");
-const webpack = require("webpack");
-
 module.exports = {
   stories: [
     "./pages/Landing.stories.mdx",
@@ -15,39 +12,4 @@ module.exports = {
     "@storybook/addon-a11y",
     "@storybook/addon-links",
   ],
-  webpackFinal: async (config) => {
-    config.module.rules = [
-      ...config.module.rules,
-      {
-        test: /\.(ts|tsx)?$/,
-        use: [
-          {
-            loader: "awesome-typescript-loader",
-            options: {
-              transpileOnly: true,
-            },
-          },
-          {
-            loader: "react-docgen-typescript-loader",
-            options: {},
-          },
-        ],
-      },
-    ];
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...(config.resolve.alias || {}),
-      },
-      extensions: [...(config.resolve.extensions || []), ".ts", ".tsx"],
-      plugins: [new TsConfigPathsPlugin({})],
-    };
-    config.plugins = [
-      ...config.plugins,
-      new webpack.DefinePlugin({
-        __DEV__: process.env.NODE_ENV === "development",
-      }),
-    ];
-    return config;
-  },
 };
