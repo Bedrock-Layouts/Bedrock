ac24a60c8b7e39febb33c73ab269debea248105c
Jarvis1010 <travis.mair@gmail.com>
2021-09-09 13:39:14 -0600

    docs(update spacing docs): update spacing docs to root

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/introduction.stories.mdx",
     "./pages/installation.stories.mdx",
     "./pages/stack.stories.mdx",
+    "./pages/spacing.stories.mdx",
     "../packages/**/examples/*.stories.(js|ts|tsx|mdx)",
     "./pages/contributing.stories.mdx",
     "../examples/examples.stories.mdx",
