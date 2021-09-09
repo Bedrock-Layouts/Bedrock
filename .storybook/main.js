939af77b9f3200c016c4a2cbf8742ce0e01a205d
Jarvis1010 <travis.mair@gmail.com>
2021-09-09 10:06:57 -0600

    docs(storybook): add more tutorial pages

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     "./pages/Landing.stories.mdx",
     "./pages/introduction.stories.mdx",
     "./pages/installation.stories.mdx",
+    "./pages/stack.stories.mdx",
     "../packages/**/examples/*.stories.(js|ts|tsx|mdx)",
     "./pages/contributing.stories.mdx",
     "../examples/examples.stories.mdx",
