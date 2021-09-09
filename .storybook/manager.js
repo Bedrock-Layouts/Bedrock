d8906d414a7ea8f5865b98dda7ac75b1d7c310c7
Jarvis1010 <travis.mair@gmail.com>
2021-09-09 14:58:51 -0600

    docs(doc ui bug): fix doc ui bug

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -12,5 +12,7 @@ addons.setConfig({
       "https://user-images.githubusercontent.com/5460770/77477816-8df68000-6de2-11ea-83be-9f12c8de7f0d.png",
     textColor: "#161e2e",
     fontBase: '"Roboto", sans-serif',
+    appBg: "white",
+    appBorderColor: "white",
   }),
 });
