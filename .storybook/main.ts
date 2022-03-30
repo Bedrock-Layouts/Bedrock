95738486866c50979d94eafaf6a6f160c9867725
Jarvis1010 <travis.mair@gmail.com>
2022-03-29 19:16:17 -0500

    docs(landing-page): mobile fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,24 @@
-module.exports = {
-  stories: [
-    "./pages/Landing.stories.mdx",
-    "./pages/introduction.stories.mdx",
-    "./pages/installation.stories.mdx",
-    "./pages/stack.stories.mdx",
-    "./pages/spacing.stories.mdx",
-    "./pages/menu.stories.mdx",
-    "./pages/hero.stories.mdx",
-    "./pages/new-arrivals.stories.mdx",
-    "../packages/**/examples/*.stories.@(js|ts|tsx|mdx)",
-    "./pages/contributing.stories.mdx",
-    "../examples/examples.stories.mdx",
-    "../examples/web.dev.stories.mdx",
-  ],
-  addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
-  },
-  staticDirs: ["../public"],
-};
+import type { Configuration } from "webpack";
+
+export const stories = [
+  "./pages/Landing.stories.mdx",
+  "./pages/introduction.stories.mdx",
+  "./pages/installation.stories.mdx",
+  "./pages/stack.stories.mdx",
+  "./pages/spacing.stories.mdx",
+  "./pages/menu.stories.mdx",
+  "./pages/hero.stories.mdx",
+  "./pages/new-arrivals.stories.mdx",
+  "../packages/**/examples/*.stories.@(js|ts|tsx|mdx)",
+  "./pages/contributing.stories.mdx",
+  "../examples/examples.stories.mdx",
+  "../examples/web.dev.stories.mdx",
+];
+
+export const addons = ["@storybook/addon-essentials"];
+
+export const framework = "@storybook/react";
+
+export const core = { builder: "webpack5" };
+
+export const staticDirs = ["../public"];
