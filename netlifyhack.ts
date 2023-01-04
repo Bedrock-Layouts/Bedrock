ecde3b63d6b1066e36747c93b855abaa4da465f5
Jarvis1010 <travis.mair@gmail.com>
2023-01-04 12:10:50 -0600

    docs(docs): fix netlify deploy

--- netlifyhack.ts
+++ netlifyhack.ts
@@ -7,5 +7,16 @@ const sbAddons = path.join(storybookStatic, "sb-addons");
 const hiddenStorybook = path.join(sbAddons, ".storybook");
 const nonHiddenStorybook = path.join(sbAddons, "storybook");
 (async () => {
-  await promisify(fs.rename)(hiddenStorybook, nonHiddenStorybook);
+  try {
+    await promisify(fs.rename)(hiddenStorybook, nonHiddenStorybook);
+    const files = await promisify(fs.readdir)(sbAddons);
+    if (!files.includes("storybook")) {
+      throw new Error("Failed to rename .storybook to storybook");
+    }
+    // eslint-disable-next-line no-console
+    console.log("Renamed .storybook to storybook");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
 })();
