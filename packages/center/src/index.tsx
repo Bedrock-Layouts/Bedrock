4e41084630d9b635f706e36352183139bf8f462d
Jarvis1010 <travis.mair@gmail.com>
2023-08-17 00:18:11 -0500

    refactor: add jsdoc style type definitions to components
    re #2010

--- packages/center/src/index.tsx
+++ packages/center/src/index.tsx
@@ -7,45 +7,67 @@ import {
 import { forwardRefWithAs } from "@bedrock-layout/type-utils";
 import React, { CSSProperties } from "react";
 
-type MaxWidth = number | CSSLength | SizesOptions;
+/**
+ * The `maxWidth` prop can accept any positive integer, `CSSLength`, `SizeOption`.
+ */
+export type MaxWidth = number | CSSLength | SizesOptions;
 
+/**
+ * Props for the Center component.
+ */
 export interface CenterProps {
+  /**
+   * Sets the max-inline size of the component.
+   * The `maxWidth` prop can accept any positive integer, `CSSLength`, `SizeOption`.
+   */
   maxWidth?: MaxWidth;
+  /**
+   * Sets the text alignment of the component to center.
+   */
   centerText?: boolean;
+  /**
+   * Sets the alignment of the component's children to be centered.
+   */
   centerChildren?: boolean;
 }
 
-export const Center = forwardRefWithAs<"div", CenterProps>(
-  ({ as, centerChildren, centerText, maxWidth, style, ...props }, ref) => {
-    const theme = useTheme();
-    const centerProps = [
-      centerText && "center-text",
-      centerChildren && "center-children",
-    ]
-      .filter((x) => x)
-      .join(" ");
+/**
+ * The `Center` component is designed to center and clamp its width at a predefined value.
+ * By default, this value is set to the `medium` breakpoint.
+ * You can also center the children and text alignment as well.
+ */
+export const Center = forwardRefWithAs<"div", CenterProps>(function Center(
+  { as, centerChildren, centerText, maxWidth, style, ...props },
+  ref,
+) {
+  const theme = useTheme();
+  const centerProps = [
+    centerText && "center-text",
+    centerChildren && "center-children",
+  ]
+    .filter((x) => x)
+    .join(" ");
 
-    const safeStyle = style ?? {};
+  const safeStyle = style ?? {};
 
-    const Component = as ?? "div";
+  const Component = as ?? "div";
 
-    return (
-      <Component
-        data-bedrock-center={centerProps}
-        ref={ref}
-        style={
-          {
-            "--maxWidth":
-              typeof maxWidth === "number" && maxWidth > 0
-                ? `${maxWidth}px`
-                : getSizeValue(theme, maxWidth) ?? maxWidth,
-            ...safeStyle,
-          } as CSSProperties
-        }
-        {...props}
-      />
-    );
-  },
-);
+  return (
+    <Component
+      data-bedrock-center={centerProps}
+      ref={ref}
+      style={
+        {
+          "--maxWidth":
+            typeof maxWidth === "number" && maxWidth > 0
+              ? `${maxWidth}px`
+              : getSizeValue(theme, maxWidth) ?? maxWidth,
+          ...safeStyle,
+        } as CSSProperties
+      }
+      {...props}
+    />
+  );
+});
 
 Center.displayName = "Center";
--- packages/columns/src/index.tsx
+++ packages/columns/src/index.tsx
@@ -9,46 +9,74 @@ import {
 import { forwardRefWithAs } from "@bedrock-layout/type-utils";
 import React, { CSSProperties } from "react";
 
+/**
+ * Props for the `Columns` component.
+ */
 export interface ColumnsProps {
+  /**
+   * Sets space between each element.
+   */
   gutter?: Gutter;
+  /**
+   * Sets the number of columns.
+   */
   columns?: number;
+  /**
+   * Sets the width breakpoint at which the columns
+   * will switch to a single column.
+   */
   switchAt?: number | CSSLength | SizesOptions;
-  children?: React.ReactNode;
 }
 
-export const Columns = forwardRefWithAs<"div", ColumnsProps>(
-  ({ as, gutter, columns = 1, style, switchAt, ...props }, ref) => {
-    const theme = useTheme();
-    const maybeGutter = getSafeGutter(theme, gutter);
-    const safeColumns = columns > 0 ? columns : 1;
-    const safeStyle = style ?? {};
-    const safeSwitchAt = getSizeValue(theme, switchAt) ?? switchAt;
+/**
+ * The `Columns` component is designed to create a n-column layout.
+ * The complimentary `Column` component will allow elements to span and
+ * offset n-columns.
+ */
+export const Columns = forwardRefWithAs<"div", ColumnsProps>(function Columns(
+  { as, gutter, columns = 1, style, switchAt, ...props },
+  ref,
+) {
+  const theme = useTheme();
+  const maybeGutter = getSafeGutter(theme, gutter);
+  const safeColumns = columns > 0 ? columns : 1;
+  const safeStyle = style ?? {};
+  const safeSwitchAt = getSizeValue(theme, switchAt) ?? switchAt;
 
-    const Component = as ?? "div";
+  const Component = as ?? "div";
 
-    return (
-      <Component
-        ref={ref}
-        data-bedrock-columns={""}
-        style={
-          {
-            ...safeStyle,
-            "--gutter": maybeGutter,
-            "--columns": safeColumns,
-            "--switchAt": safeSwitchAt,
-          } as CSSProperties
-        }
-        {...props}
-      />
-    );
-  },
-);
+  return (
+    <Component
+      ref={ref}
+      data-bedrock-columns={""}
+      style={
+        {
+          ...safeStyle,
+          "--gutter": maybeGutter,
+          "--columns": safeColumns,
+          "--switchAt": safeSwitchAt,
+        } as CSSProperties
+      }
+      {...props}
+    />
+  );
+});
 
-Columns.displayName = "Columns";
-
+/**
+ * Props for the `Column` component.
+ */
 export interface ColumnProps {
+  /**
+   * Sets the number of columns the element will span.
+   */
   span?: number;
+  /**
+   * Sets the number of columns the element will offset from the start.
+   */
   offsetStart?: number;
+  /**
+   * Sets the number of columns the element will offset from the end.
+   */
   offsetEnd?: number;
 }
 
@@ -56,30 +84,34 @@ const safeSpan = (span: unknown) => {
   return typeof span === "number" ? span : 1;
 };
 
-export const Column = forwardRefWithAs<"div", ColumnProps>(
-  ({ as, span, style, offsetStart = 0, offsetEnd = 0, ...props }, ref) => {
-    const safeOffsetStart = offsetStart > 0 ? offsetStart : undefined;
-    const safeOffsetEnd = offsetEnd > 0 ? offsetEnd : undefined;
-    const safeStyle = style ?? {};
+/**
+ * The `Column` component is designed to be used in conjunction
+ * with the `Columns` component.
+ * It allows elements to span and offset n-columns.
+ */
+export const Column = forwardRefWithAs<"div", ColumnProps>(function Column(
+  { as, span, style, offsetStart = 0, offsetEnd = 0, ...props },
+  ref,
+) {
+  const safeOffsetStart = offsetStart > 0 ? offsetStart : undefined;
+  const safeOffsetEnd = offsetEnd > 0 ? offsetEnd : undefined;
+  const safeStyle = style ?? {};
 
-    const Component = as ?? "div";
+  const Component = as ?? "div";
 
-    return (
-      <Component
-        data-bedrock-column
-        ref={ref}
-        style={
-          {
-            ...safeStyle,
-            "--span": Math.max(safeSpan(span), 1),
-            "--offsetStart": safeOffsetStart,
-            "--offsetEnd": safeOffsetEnd,
-          } as CSSProperties
-        }
-        {...props}
-      />
-    );
-  },
-);
-
-Column.displayName = "Column";
+  return (
+    <Component
+      data-bedrock-column
+      ref={ref}
+      style={
+        {
+          ...safeStyle,
+          "--span": Math.max(safeSpan(span), 1),
+          "--offsetStart": safeOffsetStart,
+          "--offsetEnd": safeOffsetEnd,
+        } as CSSProperties
+      }
+      {...props}
+    />
+  );
+});
