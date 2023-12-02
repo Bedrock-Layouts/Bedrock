d295bda0b8b85be143c68adcbf1e9b03c19aece5
Jarvis1010 <travis.mair@gmail.com>
2023-12-02 14:25:20 -0600

    docs(appboundary): change to autodocs

--- stories/appboundary/argTypes.ts
+++ stories/appboundary/argTypes.ts
@@ -1,10 +1,5 @@
 export const argTypes = {
   boundarySize: {
-    description: "Sets the max inline size of the component",
-    type: { name: "string" },
-    table: {
-      type: { summary: "number, CSSLength,, SizesOption" },
-    },
     control: "select",
     options: [
       "sizeContent1",
