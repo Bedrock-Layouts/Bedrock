247ef3affae075d97fd249673a49a2b559305dee
Jarvis1010 <travis.mair@gmail.com>
2023-12-03 00:59:54 -0600

    docs(cover): update cover to use autodocs

--- stories/cover/argTypes.ts
+++ stories/cover/argTypes.ts
@@ -1,10 +1,5 @@
 export const argTypes = {
   gutter: {
-    description: "Sets space between each element",
-    type: { name: "string", required: true },
-    table: {
-      type: { summary: "string" },
-    },
     control: "select",
     options: [
       "size000",
@@ -27,26 +22,6 @@ export const argTypes = {
     ],
   },
   minHeight: {
-    description: "Sets the minimum block size of the component",
-    type: { name: "string" },
-    table: {
-      type: { summary: "CSSLength" },
-      defaultValue: { summary: "100vh" },
-    },
     control: "text",
-  },
-  top: {
-    description: "renders an React Node before the children",
-    type: { name: "ReactNode" },
-    table: {
-      type: { summary: "ReactNode" },
-    },
-  },
-  bottom: {
-    description: "renders an React Node after the children",
-    type: { name: "ReactNode" },
-    table: {
-      type: { summary: "ReactNode" },
-    },
   },
 };
